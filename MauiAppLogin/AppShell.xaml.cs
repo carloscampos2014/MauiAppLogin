00b6bcd51406c1529a887f108ed946a0d010bded
Carlos Alberto de Lima Campos <carlosalbertode.campos@gmail.com>
2026-05-02 15:08:19 -0300

    Add project files.

---
+++ MauiAppLogin/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppLogin;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MauiAppLogin/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAppLogin;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
